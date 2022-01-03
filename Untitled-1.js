b89b28e12d62bbc12ae37c4c926827079748683d
Arup Deb <debarup20010@gmail.com>
2022-01-03 10:55:10 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,19 @@
+var x = "Namaste ",
+    y = "world";
+x = x + " " + y;
+x
+/*
+var x =9;
+--x;
+x
+*/
+
+/*
+var x = 3 !='4';
+x 
+*/
+/*var x = 52, y=10;
+x = x%y;
+
+ var a= 10, b=2; 
+ */
---
+++ js1.js
@@ -0,0 +1,15 @@
+/*
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', showMsg);
+
+function showMsg() {
+    alert("Namaste World!");
+}
+*/
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', inputMsg);
+
+function inputMsg() {
+    let name = prompt('Enter Name of Student');
+    namasteBtn.textContent = 'Roll No. 1: Arup' + name;
+}
